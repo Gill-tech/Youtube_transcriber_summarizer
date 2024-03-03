c4e9e34ac55900a79f3f8341899abc9b7f02e8ae
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-03-03 15:37:02 +0100

    launching update

--- pages/Youtube_Q&A.py
+++ pages/Youtube_Q&A.py
@@ -14,7 +14,6 @@ from summarizer import extract_transcript
 from youtube_id_extractor import extract_video_id
 from yt_chat_processing import create_chunk_and_vector_db, use_input
 
-
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -22,7 +21,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 st.set_page_config("Chat With Youtube Videos")
 
 # Page title
-st.title("YouTube Video Q & A")
+st.title("Chat with Youtube Videos using Gemini")
 
 
 ########### Functions ###########
@@ -39,93 +38,92 @@ def create_chunk_and_vector_db():
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, 
-    if the answer is not in the provided context, just say "answer is not available in the context", don't provide wrong answer.
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details and explain it better that it is in the context, 
+    if the answer is not in provided context or even out of the scope of the context just say,
+    "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
-    Question:\n {question}\n
+    Question: \n{question}\n
 
     Answer:
-
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3,
-                                   convert_system_message_to_human=True)
-    PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(model=model, chain_type="studd", prompt=prompt_template)
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3, convert_system_message_to_human=True)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
     return chain
 
-def use_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001", task_type="retrieval_document")
-
-    print("Embedding loaded")
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", task_type="retrieval_document")
+    
     new_db = FAISS.load_local("faiss_index", embeddings)
-    print('Database retrieved')
-    print("Now searching DB")
-    docs = new_db.search(user_question)
-    print('Passing to chain')
-    chain =  get_conversational_chain()
-    print('Getting answer')
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
     response = chain(
-        {"input_documents": docs, "question": user_question}, 
+        {"input_documents":docs, "question": user_question},
         return_only_outputs=True
     )
-    print(response)
-    st.write("Reply: ", response["output_text"])
 
-# a function to wrap everything
+    return response["output_text"]
+
+
+@st.cache_data
+def extract_and_process(youtube_url):
+    # Extract video ID from YouTube URL
+    youtube_id = extract_video_id(youtube_url)
+    
+    # If YouTube ID is valid
+    if youtube_id:
+
+        # Extract transcript from the YouTube video
+        transcript = extract_transcript(youtube_url)
+
+        # If transcript extraction is successful
+        if transcript != "Error":
+            # Save transcript to a text file
+            with open('transcript.txt', 'w') as f:
+                f.write(transcript)
+            # Process the transcript
+            with st.spinner("Preparing for Q & A..."):
+                create_chunk_and_vector_db()
+                st.balloons()
+        else:
+            st.write("Error getting the transcript. Please check the video URL.")
+    # Return thumbnail URL and transcript
+    return  transcript
 
 def main():
-# URL entry
-    youtube_url = st.text_input("Enter the youtube video URL or link")
+    # URL entry
+    youtube_url = st.text_input("Enter the YouTube video URL or link")
 
     # show the user the summary of the video
     st.write("Click the button below to get the summary of the video")
-    if st.button("Get Detailed Summary"):
 
-        # display the thumbnail of the video
-        st.markdown("### Video Thumbnail")
+    if st.button("process video"):
+
         if youtube_url is None or youtube_url.strip() == "":
             st.write("The input is empty, enter a valid URL.")
         else:
-            youtube_id = extract_video_id(youtube_url)
-            if youtube_id is None or youtube_id.strip() == "":
-                st.write("Invalid YouTube URL. Please enter a valid URL.")
-            else:
-                st.image(f"http://img.youtube.com/vi/{youtube_id}/0.jpg", use_column_width=True)
-
-                # a spinner to show that the transcript is being extracted
-                with st.spinner("Getting the transcript..."):
-                    # get the transcript from the youtube video
-                    transcript = extract_transcript(youtube_url)
-                    # baloon to show successful extraction of the transcript
-                    st.balloons()
-
-                # if there is an error getting the transcript, display an error message
-                if transcript == "Error":
-                    st.write("Error getting the transcript. Please check the video URL.")
-                else:
-                    # saving the transcript in a text file
-                    with open('transcript.txt', 'w') as f:
-                        f.write(transcript)
-                    
-                    # processing the transcript
-                    with st.spinner("Preparing for Q & A..."):
-                            create_chunk_and_vector_db()
-                            st.balloons()
-                            st.success("Done")
-                    # Next Phase
-                    st.header("Chat with Youtube Videos using Gemini")
-
-                    # Taking User Input
-                    user_question = st.text_input("Enter your question for the youtube video: ")
-
-                    if st.button("Submit & Process"):
-                    # Passing the question to the chain
-                        if user_question:
-                            st.write("PASSED TO CHAIN")
-                            use_input(user_question)
+            transcript = extract_and_process(youtube_url)
+            # Store state in session_state
+            st.session_state.transcript = transcript
+    
+    # Taking User Input
+    user_question = st.text_input("Enter your question for the youtube video: ")
+    if st.session_state.transcript is not None:
+        if st.button("Submit & Process"):
+            with st.spinner('Getting answer'):
+                # Passing the question to the chain
+                if user_question:
+                    response = user_input(user_question)
+                    st.markdown(f"### Response")
+                    st.write(response)
 
 
-        
+# Run the main function
 if __name__ == "__main__":
     main()
